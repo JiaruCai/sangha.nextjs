0e7cc8ebbf5f4eff57bb5822abb8a0b7c5ac3fac
Jiaru Cai <caijorievivi@gmail.com>
2024-04-30 12:49:47 -0700

    remove fonts key from mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,13 +8,6 @@ const nextConfig = {
         return config;
     },
 
-    // Add Google Fonts configuration here
-    fonts: {
-        google: {
-            families: ["Noto Serif", "Merriweather", "Open Sans"], // Add your desired font families here
-        },
-    },
-
     basePath: '', // Only set this if needed
 
     // Any other configurations can be added here
