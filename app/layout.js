8cadcc66a3631659485de730cb8a1d712dc6c67b
Jiaru Cai <caijorievivi@gmail.com>
2024-05-21 14:35:03 -0700

    updated mobile app version layout

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import "./globals.css";
 
 const inter = Inter({subsets: ["latin"]});
 
+
 export const metadata = {
     title: "Join Sangha",
     description: "Meditate today on your own schedule!",
